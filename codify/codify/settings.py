104da8bc4a2ec821b8dd1d664d4939a06e698d3e
abinash <dasabinash1807@gmail.com>
2026-03-21 00:55:15 +0530

    url add

--- codify/codify/settings.py
+++ codify/codify/settings.py
@@ -182,17 +182,15 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://codify-das15122003-4746s-projects.vercel.app"
 ]
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://your-project.vercel.app"
-]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://your-project.vercel.app"
+    "https://codify-das15122003-4746s-projects.vercel.app"
 ]
 
 
