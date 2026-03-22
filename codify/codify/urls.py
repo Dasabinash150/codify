915e60baabd4f986819ca72e2823ea2a9d4d41e5
ABINASH DAS <158310375+Dasabinash150@users.noreply.github.com>
2026-03-22 09:42:34 +0530

    Update urls.py

--- codify/codify/urls.py
+++ codify/codify/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
+def home(request):
+    return JsonResponse({
+        "message": "Codify API is running"
+    })
 
 urlpatterns = [
-
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/user/',include('account.urls')),
     path('api/',include('contest.urls')),
